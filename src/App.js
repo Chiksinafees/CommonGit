2024fde166f7a68efe71a7d66342a15eba8ca7b4
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-12-09 17:39:53 +0530

    Loading the application (task 7 of eccomerse website )

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment, useState } from "react";
 import MovieList from "./component/MoviesList";
-
+import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true);
     try {
       const response = await fetch("https://swapi.py4e.com/api/films/");
       const data = await response.json();
@@ -21,15 +23,20 @@ function App() {
     } catch (err) {
       console.log(err);
     }
+    setIsLoading(false);
   }
 
   return (
     <Fragment>
-      <section>
-        <button onClick={fetchMovieHandler}>fetch Movies</button>
+      <section className="section">
+        <button onClick={fetchMovieHandler} className="button">
+          fetch Movies
+        </button>
       </section>
       <section>
-        <MovieList movies={movies} />
+        {!isloading && movies.length > 0 && <MovieList movies={movies} />}
+        {!isloading && movies.length === 0 && <p>no movies found</p>}
+        {isloading && <p>loading...</p>}
       </section>
     </Fragment>
   );
