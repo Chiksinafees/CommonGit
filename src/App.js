3083cb4ee48b0df40d14bf8f2a0ff79429d46bd7
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-12-10 15:04:41 +0530

    Making apps error proof ( task 8 of eccomerse website )

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
 import { Fragment, useState } from "react";
 import MovieList from "./component/MoviesList";
 import "./App.css";
+
 function App() {
+
   const [movies, setMovies] = useState([]);
   const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [stopRetry, setStopRetry] = useState(true);
 
   async function fetchMovieHandler() {
-    setIsLoading(true);
-    try {
-      const response = await fetch("https://swapi.py4e.com/api/films/");
-      const data = await response.json();
 
+    console.log("run");
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/film/");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+
+      const data = await response.json();
       const transferMovies = data.results.map((moviedata) => {
         return {
           id: moviedata.episode_id,
           title: moviedata.title,
           openingText: moviedata.opening_crawl,
           releaseDate: moviedata.relese_date,
-        };
-      });
-      setMovies(transferMovies);
-    } catch (err) {
-      console.log(err);
-    }
+               };
+             });
+          setMovies(transferMovies);
+        } 
+         catch (error) {
+            setError(error.message);
+            }
+
     setIsLoading(false);
   }
+
+  if (error) {
+     if (stopRetry) {
+        setTimeout(() => {
+           fetchMovieHandler();
+            }, 5000);
+           }
+         }
+ 
+  const stopError = () => {
+    //console.log('stop')
+    setStopRetry(false);
+  };
 
   return (
     <Fragment>
@@ -32,10 +59,12 @@ function App() {
         <button onClick={fetchMovieHandler} className="button">
           fetch Movies
         </button>
+        <button onClick={stopError}>Cancel</button>
       </section>
       <section>
         {!isloading && movies.length > 0 && <MovieList movies={movies} />}
-        {!isloading && movies.length === 0 && <p>no movies found</p>}
+        {!isloading && movies.length === 0 && !error && <p>no movies found</p>}
+        {!isloading && error && <p>{error}</p>}
         {isloading && <p>loading...</p>}
       </section>
     </Fragment>
